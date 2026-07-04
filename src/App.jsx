920ae385a083ff72fa0c6d513196188538b6142a
goutham <gvgoutham551@gmail.com>
2026-07-04 22:34:23 +0530

    fix Footer import

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Services from "./components/Services";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
